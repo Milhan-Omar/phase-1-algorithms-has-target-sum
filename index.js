825a25e376f76150d7abe350452622f589a069a5
Milhan Osman <milhanomarosman@gmail.com>
2023-04-03 14:37:08 +0300

    completed assigment

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for(i=0; i < array.length; i++){
+		let comp = array.splice(0, 1); 
+		if(array.includes(target-comp)){
+			return true
+		}
+	}
+	return false
 }
 
 /* 
   Write the Big O time complexity of your function here
+it has a time complexity of O(n)
 */
 
 /* 
   Add your pseudocode here
+ 1.destructively select the first Element,
+ 2.check whether the array includes target-first element if so returns true
 */
 
 /*
-  Add written explanation of your solution here
+   Add written explanation of your solution here
+  the first loop creates identical copy of the array 
+  the second loop iterates through the array trying different combinations which add to our target 
+  if any found, it returns true, else false
 */
 
 // You can run `node index.js` to view these console logs
